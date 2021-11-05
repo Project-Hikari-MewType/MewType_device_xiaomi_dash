c1a1d0005602df0ad642c5888e97be66cafb3eaa
Arian <arian.kulmer@web.de>
2021-11-05 16:44:55 +0100

    dash: Add power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df

--- device.mk
+++ device.mk
@@ -300,6 +300,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-dash)
+
 # Radio
 ENABLE_VENDOR_RIL_SERVICE := true
 
