87923cd088f0ac158bd00cc69208cd656248d039
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-11 11:24:54 +0530

    dash: Provide liboemcrypto linker config
    Change-Id: Ib7c242be5db626ba0151bbf60dd6d60d7590bc46

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Linker
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    $(LOCAL_PATH)/configs/linker/linker.config.json
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
