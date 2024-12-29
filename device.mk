1013f79658390500e1211ca749223c6e57d4bb1f
bengris32 <bengris32@protonmail.ch>
2024-12-29 01:06:28 +0000

    dash: init: Add fingerprint init script
    Change-Id: I4ceb833539daf3e0056a9adc71ae33d99b6f651d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -187,6 +187,7 @@ PRODUCT_PACKAGES += \
     init.cgroup.rc \
     init.connectivity.common.rc \
     init.connectivity.rc \
+    init.fingerprint.rc \
     init.mi_thermald.rc \
     init.modem.rc \
     init.mt6991.rc \
