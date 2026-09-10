b879225a818d60e166a3a4bcf0a7d9a57a5c1dbc
AdarshGrewal <adarshgrewal@gmail.com>
2026-09-10 09:52:15 +0530

    dash: Setup lights aidl
    Change-Id: I39c2764248356e5d00e356cb378c4ea2d41d71e3

--- device.mk
+++ device.mk
@@ -243,6 +243,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker/linker.config.json
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.dash
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
